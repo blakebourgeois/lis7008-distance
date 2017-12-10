26c8ea7c4b5e4d4c5aa635c5a47f01ef8b888d4d
Blake Bourgeois <31666557+blakebourgeois@users.noreply.github.com>
2017-12-10 07:20:01 -0600

    v2 Working
    Updating the branch with fixed, functional code.
    
    Ran the code against the results from my initial project and it works as before but without needing external work to quickly select lowest entries for each comparison for items in $file1

--- DistanceCalculation.ps1
+++ DistanceCalculation.ps1
@@ -24,60 +24,60 @@ $bigMaster = @()
 
 ## FILE1 to FILE2
 ## our count starts at 0, we want to contine WHILE count is under the total length of the data in file1
+
 while($count -lt $file1.length){
+    ## Setting up a minimum distance. No two geo locations on earth should exceed this distance in miles so first run should always update this value
+    ## The value resets when item1[0] is incremented to item1[1] etc
+    ## Initalize a blank array to hold the description of the data (this may not be the best way to accomplish this step)
+    $minDist = 15000
+    $littleMaster = @()
+
     ## we use count to increment through each geolocation in file1
     ## we are going to use a SECOND count to increment through each geolocation in file2
-    $minDist = 1000
-    $littleMaster = @()
     while($count2 -lt $file2.length){
 
         # create the item that houses the lat and long for item in file1
         # the count is used to select the correct index
         # when the count is increased later, the file1 list item will change
         $item1 = New-Object System.Device.Location.GeoCoordinate $file1[$count].lat, $file1[$count].long
-        # create the item that houses the lat and long for the park
+        # create the item that houses the lat and long for file2
         $item2 = New-Object System.Device.Location.GeoCoordinate $file2[$count2].lat, $file2[$count2].long
         # uses the built in function getdistanceto to get the distance
         # converts that distance to km
         # converts KM to miles
         # rounds result to 2 decimal places
+        # edit the math as is appropriate for your application
         $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
-        
-        
-        # create a csv from each data point
-        # actually please don't this is awful and it doesn't work
-        # I wrote this because I needed the shortest of each comparison only
-        #$file1[$count].address + "," + $file2[$count2].address + "," + $dist | out-file C:\scripts\geo\libtobrec.csv -Append
 
-        # testing...is this better?
+        # Pull the addresses of each item for reference
         $address1 = $file1[$count].address
         $address2 = $file2[$count2].address
-        #$comparison = $dist + "," + $file1[$count].address + "," + $file2[$count2].address
+        
+        # Build the comparison string for output
+        # again this can be edited for your application as necessary, this made the most sense for my needs
         $comparison = "$dist,$address1,$address2"
-        write-host "this comparison is:"
-        $comparison
-        write-host "before update, the mindist is:"
-        $minDist
 
+        # the newly calcuated distance is compared against the mindist
+        # on first run it should always be less than 15000 so minDist will hold the first comparison
+        # when new distances are calculated, it will update minDist to the new short distance litteMaster to the new comparison string.
         if($dist -lt $minDist)
-            {write-host "minDist has been updated..."
-            $minDist = $dist
-            $minDist
-            $littlemaster = $littleMaster + $comparison}
+            {$minDist = $dist
+            $littlemaster = $comparison}
         
         # increment second count
         # will increment until we're out of items in file2
         $count2 = $count2 + 1
         }
+
    # when we finish comparing each item in file1 to file2 we'll move to the next in file1 and repeat
    $count = $count + 1
-   $littleMaster
-   $bigMaster = $bigMaster + $littleMaster
 
+   # output the shortest result for file1[x] here. This will repeat until file1 is exhausted giving a single response for each item
+   $littleMaster
    # reset count so the file2 loop runs again
    $count2 = 0
+
+   # we got what we wanted so we clear out that array for the next iteration
    $littleMaster = @()
 }
-
-$bigMaster
 
