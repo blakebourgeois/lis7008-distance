a5601fb8ffeee6a7b434bdd06a764cdc9f89b6a7
Blake Bourgeois <31666557+blakebourgeois@users.noreply.github.com>
2017-12-10 07:00:54 -0600

    Start of v2
    Moved sloppy code into old file; created new sample csv files; going to change this to a single function that accepts two files and does the full processing of results.
    
    This commit is NOT working yet but wanted to save my work and start the branch to play with GitHub

--- DistanceCalculation.ps1
+++ DistanceCalculation.ps1
@@ -1,153 +1,83 @@
-﻿## Import all our data
-$libraries = import-csv c:\scripts\geo\library.csv
-$brec = import-csv c:\scripts\geo\brec.csv
-$school = import-csv C:\scripts\geo\school.csv
+﻿##Distance Calculation 2.0
+
+## Import all our data
+
+## For 2.0 we're still reading from local files
+## For use replace $path with whatever works for your env
+## End goal is to create a function
+## get-distancecalculation -file1 path -file2 path
+
+$path = "C:\Users\Blake\Documents\GitHub\lis7008-distance"
+$file1 = import-csv "$path\file1.csv"
+$file2 = import-csv "$path\file2.csv"
 
 Add-Type -AssemblyName System.Device
- 
-## leftover test code from web example
-#$BNA = New-Object System.Device.Location.GeoCoordinate 36.12, -86.67
-#$LAX = New-Object System.Device.Location.GeoCoordinate 33.94, -118.40
 
-#$BNA.GetDistanceTo( $LAX ) / 1000
-
-
-## Set our counts. I'm lazy so instead of doing function we're copy pasting
+## Set our counts outside of loops
 $count = 0
 $count2 = 0
+$bigMaster = @()
 
-## LIBRARIES TO BREC
-## going to break down code once
-## our count starts at 0, we want to contine WHILE count is under the total length of the data in libraries list
-while($count -lt $libraries.length){
-    ## we use count to increment through each library
-    ## we are going to use a SECOND count to increment through each park
-    while($count2 -lt $brec.length){
-        # create the item that houses the lat and long for the library
+## New Function
+## Instead of the 6 iterations of this in v1 we only need a single observation
+## Real use case would be a 1 to 1 file comparison.
+
+## FILE1 to FILE2
+## our count starts at 0, we want to contine WHILE count is under the total length of the data in file1
+while($count -lt $file1.length){
+    ## we use count to increment through each geolocation in file1
+    ## we are going to use a SECOND count to increment through each geolocation in file2
+    $minDist = 1000
+    $littleMaster = @()
+    while($count2 -lt $file2.length){
+
+        # create the item that houses the lat and long for item in file1
         # the count is used to select the correct index
-        # when the count is increased later, the library will change
-        $item1 = New-Object System.Device.Location.GeoCoordinate $libraries[$count].lat, $libraries[$count].long
+        # when the count is increased later, the file1 list item will change
+        $item1 = New-Object System.Device.Location.GeoCoordinate $file1[$count].lat, $file1[$count].long
         # create the item that houses the lat and long for the park
-        $item2 = New-Object System.Device.Location.GeoCoordinate $brec[$count2].lat, $brec[$count2].long
+        $item2 = New-Object System.Device.Location.GeoCoordinate $file2[$count2].lat, $file2[$count2].long
         # uses the built in function getdistanceto to get the distance
         # converts that distance to km
         # converts KM to miles
         # rounds result to 2 decimal places
         $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
+        
+        
         # create a csv from each data point
-        $libraries[$count].address + "," + $brec[$count2].address + "," + $dist | out-file C:\scripts\geo\libtobrec.csv -Append
+        # actually please don't this is awful and it doesn't work
+        # I wrote this because I needed the shortest of each comparison only
+        #$file1[$count].address + "," + $file2[$count2].address + "," + $dist | out-file C:\scripts\geo\libtobrec.csv -Append
+
+        # testing...is this better?
+        $address1 = $file1[$count].address
+        $address2 = $file2[$count2].address
+        #$comparison = $dist + "," + $file1[$count].address + "," + $file2[$count2].address
+        $comparison = "$dist,$address1,$address2"
+        write-host "this comparison is:"
+        $comparison
+        write-host "before update, the mindist is:"
+        $minDist
+
+        if($dist -lt $minDist)
+            {write-host "minDist has been updated..."
+            $minDist = $dist
+            $minDist
+            $littlemaster = $littleMaster + $comparison}
+        
         # increment second count
-        # will increment until we're out of parks
+        # will increment until we're out of items in file2
         $count2 = $count2 + 1
         }
-   # when we finish each park we'll move to the next library
+   # when we finish comparing each item in file1 to file2 we'll move to the next in file1 and repeat
    $count = $count + 1
-   # reset count so the park loop runs again
+   $littleMaster
+   $bigMaster = $bigMaster + $littleMaster
+
+   # reset count so the file2 loop runs again
    $count2 = 0
+   $littleMaster = @()
 }
 
-## LIBRARIES TO SCHOOL
-$count = 0
-$count2 = 0
+$bigMaster
 
-while($count -lt $libraries.length){
-    $count
-    while($count2 -lt $school.length){
-        $item1 = New-Object System.Device.Location.GeoCoordinate $libraries[$count].lat, $libraries[$count].long
-        $item2 = New-Object System.Device.Location.GeoCoordinate $school[$count2].lat, $school[$count2].long
-        $libraries[$count].address
-        $school[$count2].address
-        $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
-        $dist
-        write-host "---"
-        $libraries[$count].address + "," + $school[$count2].address + "," + $dist | out-file C:\scripts\geo\libtoschool.csv -Append
-        $count2 = $count2 + 1
-        }
-   $count = $count + 1
-   $count2 = 0
-}
-
-## BREC TO LIBRARIES
-$count = 0
-$count2 = 0
-
-while($count -lt $brec.length){
-    $count
-    while($count2 -lt $libraries.length){
-        $item1 = New-Object System.Device.Location.GeoCoordinate $brec[$count].lat, $brec[$count].long
-        $item2 = New-Object System.Device.Location.GeoCoordinate $libraries[$count2].lat, $libraries[$count2].long
-        $brec[$count].address
-        $libraries[$count2].address
-        $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
-        $dist
-        write-host "---"
-        $brec[$count].address + "," + $libraries[$count2].address + "," + $dist | out-file C:\scripts\geo\brectolib.csv -Append
-        $count2 = $count2 + 1
-        }
-   $count = $count + 1
-   $count2 = 0
-}
-
-## BREC TO SCHOOLS
-$count = 0
-$count2 = 0
-
-while($count -lt $brec.length){
-    $count
-    while($count2 -lt $school.length){
-        $item1 = New-Object System.Device.Location.GeoCoordinate $brec[$count].lat, $brec[$count].long
-        $item2 = New-Object System.Device.Location.GeoCoordinate $school[$count2].lat, $school[$count2].long
-        $brec[$count].address
-        $school[$count2].address
-        $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
-        $dist
-        write-host "---"
-        $brec[$count].address + "," + $school[$count2].address + "," + $dist | out-file C:\scripts\geo\brectoschool.csv -Append
-        $count2 = $count2 + 1
-        }
-   $count = $count + 1
-   $count2 = 0
-}
-
-## SCHOOLS TO LIBRARIES
-$count = 0
-$count2 = 0
-
-while($count -lt $school.length){
-    $count
-    while($count2 -lt $libraries.length){
-        $item1 = New-Object System.Device.Location.GeoCoordinate $school[$count].lat, $school[$count].long
-        $item2 = New-Object System.Device.Location.GeoCoordinate $libraries[$count2].lat, $libraries[$count2].long
-        $school[$count].address
-        $libraries[$count2].address
-        $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
-        $dist
-        write-host "---"
-        $school[$count].address + "," + $libraries[$count2].address + "," + $dist | out-file C:\scripts\geo\schooltolib.csv -Append
-        $count2 = $count2 + 1
-        }
-   $count = $count + 1
-   $count2 = 0
-}
-
-## SCHOOLS TO BREC
-
-$count = 0
-$count2 = 0
-
-while($count -lt $school.length){
-    $count
-    while($count2 -lt $brec.length){
-        $item1 = New-Object System.Device.Location.GeoCoordinate $school[$count].lat, $school[$count].long
-        $item2 = New-Object System.Device.Location.GeoCoordinate $brec[$count2].lat, $brec[$count2].long
-        $school[$count].address
-        $brec[$count2].address
-        $dist = [math]::Round((($item1.GetDistanceTo($item2) / 1000) * 0.621371),2)
-        $dist
-        write-host "---"
-        $school[$count].address + "," + $brec[$count2].address + "," + $dist | out-file C:\scripts\geo\schooltobrec.csv -Append
-        $count2 = $count2 + 1
-        }
-   $count = $count + 1
-   $count2 = 0
-}
